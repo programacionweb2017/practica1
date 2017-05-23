3397b40a2632255912f4e763c9b4b8550e0d5e8c
Sindy <badsindy56@gmail.com>
2017-05-23 18:31:42 -0500

    art 5

--- src/Curso/MainBundle/Controller/DefaultController.php
+++ src/Curso/MainBundle/Controller/DefaultController.php
@@ -24,6 +24,7 @@ class DefaultController extends Controller
               array('id' => 2, 'title' => 'Articulo numero 2', 'created' => '2011-01-01'),
               array('id' => 3, 'title' => 'Articulo numero 3', 'created' => '2011-01-01'),
               array('id' => 4, 'title' => 'Articulo numero 4', 'created' => '2011-01-01'),
+              array('id' => 5, 'title' => 'Articulo numero 5', 'created' => '2011-01-01'),
           );
           return $this->render("CursoMainBundle:Default:articulos.html.twig", array('articulos' => $articulos));
     }
